5ca455103dd6a857ce3239af90ae1850779ede3b
Soroush Safarzade <safarzade.soroush@gmail.com>
2023-12-08 18:34:54 +0000

    Revert "Revert to 1st Commit!"
    This reverts commit b499f1b111ca028200c02923da5f443e8e1a251c.

--- script1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Hello World!"
-
-echo "This is script1.sh"
--- script2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "The currect day is: "`date +%Y/%m/%d`
-
-echo "The currect time is: "`date +%H:%M:%S`
